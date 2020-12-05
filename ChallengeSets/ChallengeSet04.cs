4e41533ef10d82582143ce51574e0fd8af8ede89
Kenya Bogans <kbogans@hotmail.com>
2020-12-04 20:12:19 -0600

    solved GetLengthOfShortestString

--- ChallengeSets/ChallengeSet04.cs
+++ ChallengeSets/ChallengeSet04.cs
@@ -1,26 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 namespace ChallengeSets
 {
     public class ChallengeSet04
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            foreach (int i in numbers)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+                else
+                {
+                    sum -= i;
+                }
+            }
+            return sum;
+
+            //throw new NotImplementedException();
         }
+
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            var list = new List<int> { str1.Length, str2.Length, str3.Length, str4.Length };
+            return list.Min();
+
+            //throw new NotImplementedException();
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
+            //var list = new List<int> { number1, number2, number3, number4 };
+            //return list.Min();
+
             throw new NotImplementedException();
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            //biz.Name = "TrueCoders";
+            //throw new NotImplementedException();
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
@@ -30,8 +54,11 @@ namespace ChallengeSets
 
         public bool IsStringANumber(string input)
         {
+           // Double result;
+            //return Double.TryParse(input, out result);
             throw new NotImplementedException();
         }
+
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
@@ -47,5 +74,6 @@ namespace ChallengeSets
         {
             throw new NotImplementedException();
         }
+
     }
 }
